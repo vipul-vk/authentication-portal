0ee3b4df9ff00f0d2fb5bfd264d1c79f589f8761
vipul-vk <vipul.khare.vk@gmail.com>
2024-06-28 13:24:35 +0530

    implemented first login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
+import LoginPage from './landing/loginPage'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world Hi
-        
-      </h1>
+      <h1>Login Page</h1>
+      <LoginPage />
     </div>
   );
 }
